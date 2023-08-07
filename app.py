9bd2f7745f0c447c50bed2c78bfb566fc86fc108
Denis <dngaspar5@gmail.com>
2023-08-07 18:10:49 +0900

    test https server with socket server

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 from flask_socketio import send, emit
 from flask_cors import CORS
+import uuid
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -14,8 +15,22 @@ def ack():
 
 @socketio.on("messageFromExtension")
 def handle_extension(data):
+    print("messageFromExtension", "data")
+    emit("messageToExtension", {"abc" :"first load from flask"}, callback=ack)
+
+@socketio.on("first")
+def handle_exten(data):
     print("messageFromExtension", data)
-    emit("messageToExtension", data, callback=ack)
+    emit("create_anonaddy", {"name": "denis", "id": str(uuid.uuid4())})
+    # emit("secondfd", {"abc" :"first load from flask"})
+
+@socketio.on("send_anonaddy")
+def recieve_anonaddy(data):
+    print(data)
+
+@socketio.on("backto")
+def handle_backto(data):
+    print("backto", data)
 
 if __name__ == '__main__':
     socketio.run(app)
